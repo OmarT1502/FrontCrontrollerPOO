d68a78fe5fab9dd939215609bb3a481c609a3472
OmarT1502 <l21390389@chetumal.tecnm.mx>
2025-05-03 18:30:26 -0500

    adding & editing new recourses

--- Producto.php
+++ Producto.php
@@ -1,37 +1,60 @@
 <?php
-	class Producto {
+	class producto{
+		// property declaration
+	    public $var = 'a default value';
+	    	// method declaration
 
-		function getproducto($id){
-			return "<br/>Se ejecutó getproducto: {$id} <br/>";
-		}
+	    //-------------------------------
+	    //No se puede sobrecargar métodos en php
 
-		function postproducto($obj) {
-			return "<br/>Se ejecutó postProducto: {$obj} <br/>";
-		}
+	    /*
+	    public static function get($id) {
+	        echo "Ejecutaste persona::get({$id})";
+	    }
+	    
+	    public static function get($idIni, $idFin) {
+	        echo "Ejecutaste persona::get({$id})";
+	    }
 
-		function deleteproducto($id){
-			return "<br/>Se ejecutó deleteproducto <br/>";
-		}
+	    public static function get() {
+	        echo "Ejecutaste persona::get({$id})";
+	    }
+		*/
 
-		function putproducto($obj){
-			return "<br/>Se ejecutó putproducto <br/>";
-		}
+	    //-----------------
+
+	    public static function getId($id) {
+	        echo "Ejecutaste producto::getId({$id})";
+	    }
+	    
+	    public static function getMany($idIni, $idFin) {
+	        echo "Ejecutaste producto::getMany({$idIni} {$idFin})";
+	    }
+
+	    public static function getAll() {
+	        echo "Ejecutaste producto::getAll()";
+	    }
+
+	    // Otro escenario: Un método único get que recibe un solo parámetro de tipo array
+
+	    /*
+	    public static function get($arrayparams) {
+
+	    	if (count($arrayparams) == 0)
+					self::getAll();
+				else  
+					if (count($arrayparams) == 1)
+						self::getId($parameters[0]);
+					else
+						if (count($arrayparams) == 2)
+							self::getMany($parameters[0], $parameters[1]);
+						//else return;
+							
+	        echo "Ejecutaste persona::get({$id})";
+	    }
+	    */
+
 	}
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
